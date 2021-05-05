eec6bed9dd35f96bec3b40075964bb5c0dc8dcb0
Nicholas V Frangione <nfrangione8@gmail.com>
2021-05-05 09:57:32 -0700

    commit tree 6a51c5a49431f6dc2299130a1b804f5f6ca20e1d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: {birds: birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    #render json: birds 
+    #@birds = Bird.all
+    #render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    #render json: @birds
+    #render json: ['As','well','as','arrays']
+    #render json: { message: 'Hashes of data will get converted to JSON' }
+    #render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+    #render plain: "Hello #{@birds[3].name}"
+    #render 'birds/index.html.erb'
   end
 end
